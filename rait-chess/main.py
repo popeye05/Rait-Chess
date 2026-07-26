fdef6561015f058ee6b429b05ec0d45867365fa9
popeye05 <swarnab.popeye@outlook.com>
2026-07-27 01:23:03 +0530

    Added CORS polixy

--- rait-chess/main.py
+++ rait-chess/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from coach_test import get_coaching_feedback, stockfish_engine, maia_engine, client #i.e using the coach_test.py here for the functions we've built!!!
-
+#phase 5: CORS Policy Addition
+from fastapi.middleware.cors import CORSMiddleware
 #Now the actual part:
 from pydantic import BaseModel
 class MoveRequest(BaseModel):
@@ -8,6 +9,13 @@ class MoveRequest(BaseModel):
     user_move: str
 
 app = FastAPI()
+#CORS addition
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
